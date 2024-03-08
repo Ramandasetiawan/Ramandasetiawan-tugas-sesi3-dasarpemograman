e1905db274fedbb534824671a5fd16d5a0ef06aa
Ramandasetiawan <160981847+Ramandasetiawan@users.noreply.github.com>
2024-03-08 11:00:06 +0700

    Add files via upload

---
+++ input-nilai.py
@@ -0,0 +1,12 @@
+nilai = int(input("Masukan Nilai : "))
+
+if nilai <=50:
+    hasil = "E"
+elif nilai <=60:
+    hasil = "D"
+elif nilai <=70:
+    hasil = "B"
+else:
+    hasil = "A"
+    
+print(hasil)
